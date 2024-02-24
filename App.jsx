733dfb0e02ae18bcd64f8faa732d96a1848c2332
Louis Gardin <louis.gardin@etudiant.univ-reims.fr>
2024-02-24 10:45:38 +0100

    Correcting delete

--- App.jsx
+++ App.jsx
@@ -14,13 +14,19 @@ export default function App() {
   }
 
   const onDelete = (id) => {
-    setItems(items.filter(item => item.id == id));
+    setItems(() => {
+      const tab = [];
+      items.forEach(item => {
+        if(item.id != id) tab.push(item);
+      });
+      return tab;
+    });
   }
 
   const renderItem = ({item}) => {
     return (
       <ScrollView>
-        <ToDoItem name={item.name} onDelete={onDelete}/>
+        <ToDoItem id={item.id}name={item.name} onDelete={onDelete}/>
       </ScrollView>
     )
   }
@@ -28,7 +34,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <AddingArea text={text} onPressButton={onPress} onChangeInput={onChangeText}/>
-      <FlatList data={items} renderItem={renderItem}/>
+      <FlatList style={{marginBottom:50}} data={items} renderItem={renderItem}/>
     </View>
   );
 }
@@ -37,6 +43,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e0e1dd',
-    marginBottom: 50,
   },
 });
